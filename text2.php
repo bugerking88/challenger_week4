6545ae294f316b81da39c8b7df65ad95ff930204
rain_wang <lion15200@gmail.com>
2016-08-22 09:12:00 +0000

    把字串印出來

--- text2.php
+++ text2.php
@@ -22,5 +22,47 @@ for ($set = 0; $set < 1200; $set++) {
     }
 }
 
+//演算法
+for($a = 0; $a < $mapHight; $a++) {
+    for($b = 0; $b < $mapWeight; $b++) {
+        if ($array[$a][$b]=="M") {
+            if($array[$a-1][$b]!="M") {
+                $array[$a-1][$b]++;
+            }
+            if($array[$a-1][$b-1]!="M") {
+                $array[$a-1][$b-1]++;
+            }
+            if($array[$a][$b-1]!="M") {
+                $array[$a][$b-1]++;
+            }
+            if($array[$a+1][$b-1]!="M") {
+                $array[$a+1][$b-1]++;
+            }
+            if($array[$a+1][$b]!="M") {
+                $array[$a+1][$b]++;
+            }
+            if($array[$a+1][$b+1]!="M") {
+                $array[$a+1][$b+1]++;
+            }
+            if($array[$a][$b+1]!="M") {
+                $array[$a][$b+1]++;
+            }
+            if($array[$a-1][$b+1]!="M") {
+                $array[$a-1][$b+1]++;
+            }
+        }
+    }
+}
+
+//印出字串
+for ($a = 0; $a < $mapHight; $a++) {
+    for ($b = 0; $b < $mapWeight; $b++) {
+        echo $array[$a][$b];
+    }
+    if ($a != 49) {
+        echo "N";
+    }
+}
+
 // $time2 = microtime(true);
 // echo "$time2"-"$time1";
