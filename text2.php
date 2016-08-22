d9c67f250e5ad7fb94de9e0e64f3bd5ccc3e61fd
rain_wang <lion15200@gmail.com>
2016-08-22 08:41:14 +0000

    第二題

---
+++ text2.php
@@ -0,0 +1,26 @@
+<?php
+
+$time1 = microtime(true);
+$mapHight = 50;
+$mapWeight = 60;
+
+for ($a = 0; $a < $mapHight; $a++) {
+    for ($b = 0; $b < $mapWeight; $b++) {
+        $array[$a][$b] = "0";
+    }
+}
+
+//放炸彈
+for ($set = 0; $set < 1200; $set++) {
+    while (true) {
+        $col = rand(0, 49);
+        $row = rand(0, 59);
+        if ($array[$row][$col] != "M") {
+            $array[$row][$col] = "M";
+            break;
+        }
+    }
+}
+
+$time2 = microtime(true);
+echo "$time2"-"$time1";
