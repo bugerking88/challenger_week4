0027a2924d2df1f68e9a87ce3f1fe697fa0135a6
rain_wang <lion15200@gmail.com>
2016-08-23 05:26:49 +0000

    第三題修正判斷

--- text3.php
+++ text3.php
@@ -1,14 +1,14 @@
 <?php
 
 header("Content-Type: text/html; charset=utf-8");
-// $_GET = $_GET['map'];
-$_GET = "M2MMMMM100N123M6M5321N12435M5MM1N1MMM4MM332N34546M423MNMM3MMM42MMN333M6M4M5MNM2113M434MNM42113M4M4N2MM102M4MM";
+
+$map = $_GET['map'];
 $mapHight = 10;
 $mapWeight = 10;
-$returnArray = explode('N', $_GET);
-$contStr = strlen($_GET);
+$returnArray = explode('N', $map);
+$contStr = strlen($map);
 
-//判斷$_GET給的長度是否正確
+//判斷$map給的長度是否正確
 if ($contStr == 109) {
     echo "字串符合長度"."<br>";
 } else {
@@ -19,22 +19,20 @@ if ($contStr == 109) {
 //建立預備陣列
 for ($a = 0; $a < $mapHight; $a++) {
     for ($b = 0; $b < $mapWeight; $b++) {
-        $preArray[$a][$b] = 0;
-    }
-}
-for ($x=0; $x<$mapHight; $x++){
-    for ($y=0; $y<$mapWeight; $y++) {
-        $preArray[$x][$y] = str_split($returnArray[$y]);
+        $tryArray[$a][$b] = "0";
     }
 }
 
-// var_dump($preArray);
-// print_r($returnArray);
+for ($x=0; $x<$mapHight; $x++){
+    // for ($y=0; $y<$mapWeight; $y++) {
+        $preArray[$x] = str_split($returnArray[$x]);
+    // }
+}
 
 //判斷有幾顆炸彈
 for ($a = 0; $a < $mapHight; $a++) {
     for ($b = 0; $b < $mapWeight; $b++) {
-        if ($preArray[0][$a][$b] == "M") {
+        if ($preArray[$a][$b] == "M") {
             $count++;
         }
     }
@@ -46,49 +44,57 @@ if($count == 40) {
     exit();
 }
 
-//演算法
-for($a = 0; $a < $mapHight; $a++) {
-    for($b = 0; $b < $mapWeight; $b++) {
-        if ($preArray[0][$a][$b]=="M") {
-            if($preArray[0][$a-1][$b]!="M") {
-                $preArray[0][$a-1][$b]--;
-            }
-            if($preArray[0][$a-1][$b-1]!="M") {
-                $preArray[0][$a-1][$b-1]--;
-            }
-            if($preArray[0][$a][$b-1]!="M") {
-                $preArray[0][$a][$b-1]--;
-            }
-            if($preArray[0][$a+1][$b-1]!="M") {
-                $preArray[0][$a+1][$b-1]--;
-            }
-            if($preArray[0][$a+1][$b]!="M") {
-                $preArray[0][$a+1][$b]--;
-            }
-            if($preArray[0][$a+1][$b+1]!="M") {
-                $preArray[0][$a+1][$b+1]--;
-            }
-            if($preArray[0][$a][$b+1]!="M") {
-                $preArray[0][$a][$b+1]--;
-            }
-            if($preArray[0][$a-1][$b+1]!="M") {
-                $preArray[0][$a-1][$b+1]--;
-            }
+//陣列歸零
+for ($a = 0; $a < $mapHight; $a++) {
+    for ($b = 0; $b < $mapWeight; $b++) {
+        if ($preArray[$a][$b] != "M") {
+            $preArray[$a][$b] = "0";
         }
     }
 }
 
+//演算法
+for($a = 0; $a < $mapHight; $a++) {
+    for($b = 0; $b < $mapWeight; $b++) {
+        if ($preArray[$a][$b]=="M") {
+            if($preArray[$a-1][$b]!="M") {
+                $preArray[$a-1][$b]++;
+            }
+            if($preArray[$a-1][$b-1]!="M") {
+                $preArray[$a-1][$b-1]++;
+            }
+            if($preArray[$a][$b-1]!="M") {
+                $preArray[$a][$b-1]++;
+            }
+            if($preArray[$a+1][$b-1]!="M") {
+                $preArray[$a+1][$b-1]++;
+            }
+            if($preArray[$a+1][$b]!="M") {
+                $preArray[$a+1][$b]++;
+            }
+            if($preArray[$a+1][$b+1]!="M") {
+                $preArray[$a+1][$b+1]++;
+            }
+            if($preArray[$a][$b+1]!="M") {
+                $preArray[$a][$b+1]++;
+            }
+            if($preArray[$a-1][$b+1]!="M") {
+                $preArray[$a-1][$b+1]++;
+            }
+        }
+
+    }
+}
+
 //判斷是否為正確地雷地圖
-$chk = 0;
 for ($a = 0; $a < $mapHight; $a++) {
     for ($b = 0; $b < $mapWeight; $b++) {
-        echo $preArray[0][$a][$b]." ";
-        if ((string)$preArray[0][$a][$b] == "M") {
-            $chk++;
-        }
-        // if ($preArray[0][$a][$b] == 0) {
-        //     $chk++;
-        // }
-    }echo "<br>";
+        $result .= $preArray[$a][$b];
+    }
+    if ($a != 9) {
+        $result .= "N";
+    }
 }
-echo "<br>".$chk;
+if ($result == $map) {
+    echo "正確";
+}
